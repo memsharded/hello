227df5748a50eefb4e1974af9683bb7853181598
AmirahSmn <37636601+AmirahSmn@users.noreply.github.com>
2019-11-17 21:42:55 +0300

    continent
    i have tried to make it a little bit better by adding the continents.

--- hello.cpp
+++ hello.cpp
@@ -3,4 +3,12 @@
 
 void hello(){
 	std::cout << "Hello World!\n";
+		std::cout << "hello Africa!\n";
+			std::cout << "hello Asia!\n";
+		std::cout << "hello Australi!\n";
+		std::cout << "hello America!\n";
+		std::cout << "hello Europe!\n";
+		std::cout << "hello Africa!\n";
+
+
 }
